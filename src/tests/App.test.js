8ea50efee99f4a2d3c99129907ab3de802ab1b7e
Marcos Pimentel <mpc_marcos@hotmail.com>
2023-03-18 18:46:58 -0300

    requisito 1.1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+it('Teste se o topo da aplicação contém um conjunto fixo de links de navegação:', () => {
+  renderWithRouter(<App />);
+  const homeLink = screen.getByRole('link', {
+    name: /home/i,
+  });
+  const aboutLink = screen.getByRole('link', {
+    name: /about/i,
+  });
+  const favoriteLink = screen.getByRole('link', {
+    name: /Favorite Pokémon/i,
+  });
+  expect(homeLink).toBeInTheDocument();
+  expect(aboutLink).toBeInTheDocument();
+  expect(favoriteLink).toBeInTheDocument();
+});
