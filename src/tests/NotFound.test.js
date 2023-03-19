683ae5058a732225c2d5d2f27428837e1261eff0
Marcos Pimentel <mpc_marcos@hotmail.com>
2023-03-18 22:04:47 -0300

    requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { act, screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+it('a página contém um heading h2 com o texto Page requested not found e se a página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
+  const { history } = renderWithRouter(<App />);
+  act(() => {
+    history.push('/pagina/naoexiste');
+  });
+  const title = screen.getByRole('heading', {
+    name: /page requested not found/i,
+    level: 2,
+  });
+  const img = screen.getByRole('img', {
+    name: /pikachu crying because the page requested was not found/i,
+  });
+  expect(title).toBeDefined();
+  expect(img).toHaveProperty(
+    'src',
+    'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif',
+  );
+});
