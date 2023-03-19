ddde31c756a35529a5a701d00dd82de2b8f03f5a
Marcos Pimentel <mpc_marcos@hotmail.com>
2023-03-18 21:40:30 -0300

    requisito 3

--- src/tests/FavoritePokemon.test.js
+++ src/tests/FavoritePokemon.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+import { act, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+it('é exibida na tela a mensagem No favorite pokemon found, caso a pessoa não tenha Pokémon favoritos', () => {
+  const { history } = renderWithRouter(<App />);
+  const favoriteLink = screen.getByRole('link', {
+    name: /Favorite Pokémon/i,
+  });
+  expect(favoriteLink).toBeInTheDocument();
+  userEvent.click(favoriteLink);
+  expect(screen.getByText(/no favorite pokémon found/i)).toBeInTheDocument();
+  act(() => {
+    history.push('/pokemon/25');
+  });
+  const favoriteInput = screen.getByRole('checkbox', {
+    name: /pokémon favoritado\?/i,
+  });
+  userEvent.click(favoriteInput);
+  userEvent.click(favoriteLink);
+  const poke = screen.getByText(/pikachu/i);
+  expect(poke).toBeInTheDocument();
+});
